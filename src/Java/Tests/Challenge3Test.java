65cc2d3b348c5420abc00471cb40ea5e191528eb
Ian Hauser <ian.hauser@dev-pro.ca>
2025-09-03 13:24:29 -0600

    reformat

--- src/Java/Tests/Challenge3Test.java
+++ src/Java/Tests/Challenge3Test.java
@@ -7,7 +7,16 @@ public class Challenge3Test implements ChallengeTest{
         @Override
         public int runTests() {
             String[] inputs = {
-                    "Hello", "xyz", "AEIOU", "banana", "PYTHON", "DevProPondersPoolPartyPurchases", "LaurieLikesLettuceLoads", "DalinDoesntDoDodgeball", "NickeroniNeverNeedsNinjas", ""
+                    "Hello",
+                    "xyz",
+                    "AEIOU",
+                    "banana",
+                    "PYTHON",
+                    "DevProPondersPoolPartyPurchases",
+                    "LaurieLikesLettuceLoads",
+                    "DalinDoesntDoDodgeball",
+                    "NickeroniNeverNeedsNinjas",
+                    ""
             };
             Integer[] expectedResults = {
                     2, 0, 5, 3, 1, 10, 11, 8, 10, 0
